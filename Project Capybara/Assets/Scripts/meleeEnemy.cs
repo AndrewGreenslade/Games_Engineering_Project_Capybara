5c0ac0621f196dc920874e1286092f37883ca618
DonalHowe <donalhowe@gmail.com>
2022-12-01 12:48:33 +0000

    added the cat to follow player

--- Project Capybara/Assets/Scripts/meleeEnemy.cs
+++ Project Capybara/Assets/Scripts/meleeEnemy.cs
@@ -9,8 +9,9 @@ public class meleeEnemy : INpc
 
  
     private bool m_movingRight;
-  
+    private bool isMovingLeft;
     private bool m_movingIdle;
+    private bool flip = false;
     int speed = 2;
   
     float m_detectionRange = 3.5f;
@@ -33,35 +34,39 @@ public class meleeEnemy : INpc
             relativePos = m_capyTransform.position - transform.position;
             if(relativePos.y < 0)
             {
-                transform.position -= Vector3.up * speed * Time.deltaTime;
+               
+                transform.position += Vector3.down * speed * Time.deltaTime;
               
-                m_movingRight = true;
-                m_movingIdle = false;
             }
-             if (relativePos.y > 0)
+            else if (relativePos.y > 0)
             {
-                transform.position -= Vector3.down * speed * Time.deltaTime;
-                //moving right
-                m_movingRight = true;
-                m_movingIdle = false;
+               
+                transform.position += Vector3.up * speed * Time.deltaTime;
+               
             }
 
             if (relativePos.x > 0)
             {
-               //flip the sprite here
-                transform.position -= Vector3.left * speed * Time.deltaTime;
-              
+                //cat is right the player
+                //flip the sprite here
+                transform.position += Vector3.right * speed * Time.deltaTime;
                 m_movingRight = true;
                 m_movingIdle = false;
+                isMovingLeft = false;
             }
-            if (relativePos.x < 0)
+            else if (relativePos.x < 0)
             {
+                //cat is right the player
                 //flipped anim cause it will look better when shooting
-                transform.position -= Vector3.right * speed * Time.deltaTime;
-                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-
-                 
-                m_movingRight = true;
+                transform.position += Vector3.left * speed * Time.deltaTime;
+                if(flip==false)
+                {
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                    flip = true;
+                }
+              
+                isMovingLeft = true;
+                m_movingRight = false;
                 m_movingIdle = false;
             }
 
@@ -69,30 +74,43 @@ public class meleeEnemy : INpc
         }
         else
         {
-       
             m_movingRight = false;
             m_movingIdle = true;
+            isMovingLeft = false;
+        }
+        if(flip==true&& m_movingIdle==true)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            flip=false;
         }
 
     }
     public override void Animate()
     {
-       
-        anim.SetBool("isMovingRight", false);
-    
-      
         anim.SetBool("isIdle", false);
-
-
+        anim.SetBool("isMovingRight", false);
        
-      
+        if (isMovingLeft==true)
+        {
+            //isMovingLeft = true;
+         
+            anim.SetBool("isMovingRight", false);
+            anim.SetBool("isIdle", false);
+
+        }
         if (m_movingRight == true)
         {
             anim.SetBool("isMovingRight", true);
+            anim.SetBool("isIdle", false);
+            isMovingLeft = false;
+            flip = false;
         }
         if (m_movingIdle == true)
         {
             anim.SetBool("isIdle", true);
+            anim.SetBool("isMovingRight", false);
+            isMovingLeft = false;
+            flip = false;
         }
     }
     public void Update()
