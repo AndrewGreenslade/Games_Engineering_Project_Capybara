f6714d2112e74e43efe71d2eb1e60c67d2cf6f20
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2022-12-01 14:30:32 +0000

    Revert "Merge branch 'cat'"
    This reverts commit f5a0d9101bf5497afcf461ae0eceb4ed01b20a8c, reversing
    changes made to 9e07a0a2d916fbc9967082d79c590ca01e8b3d1c.

--- Project Capybara/Assets/Scripts/meleeEnemy.cs
+++ Project Capybara/Assets/Scripts/meleeEnemy.cs
@@ -6,18 +6,16 @@ public class meleeEnemy : INpc
 {
     public Animator anim;
     public Transform m_capyTransform;
-    public GameObject attackObject;
-    private bool AttackPlayer = false;
+
+ 
     private bool m_movingRight;
-    private bool isMovingLeft;
+  
     private bool m_movingIdle;
-    private bool flip = false;
-    private bool playerHasAttacked = false;
     int speed = 2;
-    float timerForAttackAlive;
+  
     float m_detectionRange = 3.5f;
     public Vector3 relativePos;
-    
+  
    
    
     public override void Health()
@@ -27,50 +25,43 @@ public class meleeEnemy : INpc
 
     public override void movement()
     {
-       
 
+        
         if (Vector3.Distance(m_capyTransform.position, transform.position) <= m_detectionRange)
         {
-           
-            timerForAttackAlive -= Time.deltaTime;
-            AttackPlayer = true;
 
             relativePos = m_capyTransform.position - transform.position;
             if(relativePos.y < 0)
             {
-               
-                transform.position += Vector3.down * speed * Time.deltaTime;
+                transform.position -= Vector3.up * speed * Time.deltaTime;
               
+                m_movingRight = true;
+                m_movingIdle = false;
             }
-            else if (relativePos.y > 0)
+             if (relativePos.y > 0)
             {
-               
-                transform.position += Vector3.up * speed * Time.deltaTime;
-               
+                transform.position -= Vector3.down * speed * Time.deltaTime;
+                //moving right
+                m_movingRight = true;
+                m_movingIdle = false;
             }
 
             if (relativePos.x > 0)
             {
-                //cat is right the player
-                //flip the sprite here
-                transform.position += Vector3.right * speed * Time.deltaTime;
+               //flip the sprite here
+                transform.position -= Vector3.left * speed * Time.deltaTime;
+              
                 m_movingRight = true;
                 m_movingIdle = false;
-                isMovingLeft = false;
             }
-            else if (relativePos.x < 0)
+            if (relativePos.x < 0)
             {
-                //cat is right the player
                 //flipped anim cause it will look better when shooting
-                transform.position += Vector3.left * speed * Time.deltaTime;
-                if(flip==false)
-                {
-                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-                    flip = true;
-                }
-              
-                isMovingLeft = true;
-                m_movingRight = false;
+                transform.position -= Vector3.right * speed * Time.deltaTime;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+                 
+                m_movingRight = true;
                 m_movingIdle = false;
             }
 
@@ -78,49 +69,30 @@ public class meleeEnemy : INpc
         }
         else
         {
-            timerForAttackAlive = 0.5f;
+       
             m_movingRight = false;
             m_movingIdle = true;
-            isMovingLeft = false;
-        }
-        if(flip==true&& m_movingIdle==true)
-        {
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
-            flip=false;
-        }
-        if(AttackPlayer==true)
-        {
-            hitPlayer();
-            AttackPlayer = false;
         }
 
     }
     public override void Animate()
     {
-        anim.SetBool("isIdle", false);
-        anim.SetBool("isMovingRight", false);
        
-        if (isMovingLeft==true)
-        {
-            //isMovingLeft = true;
-         
-            anim.SetBool("isMovingRight", false);
-            anim.SetBool("isIdle", false);
+        anim.SetBool("isMovingRight", false);
+    
+      
+        anim.SetBool("isIdle", false);
 
-        }
+
+       
+      
         if (m_movingRight == true)
         {
             anim.SetBool("isMovingRight", true);
-            anim.SetBool("isIdle", false);
-            isMovingLeft = false;
-            flip = false;
         }
         if (m_movingIdle == true)
         {
             anim.SetBool("isIdle", true);
-            anim.SetBool("isMovingRight", false);
-            isMovingLeft = false;
-            flip = false;
         }
     }
     public void Update()
@@ -142,27 +114,7 @@ public class meleeEnemy : INpc
 
     public override void hitPlayer()
     {
-       
-        if (playerHasAttacked==false)
-        {
-            GameObject cloneAttack = Instantiate(attackObject, gameObject.transform.position, Quaternion.identity);
-            Destroy(cloneAttack, 0.5f);
-            playerHasAttacked = true;
-            timerForAttackAlive = 1.5f;
-
-        }
-        
-         
-
-        if (timerForAttackAlive <= 0.03f)
-        {
-            playerHasAttacked = false;
-        }
-        else
-        {
-            playerHasAttacked= true;
-        }
-
+        Debug.Log("cat enemy hitplayer");
     }
 
 }
