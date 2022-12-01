c276cd1fc0228537109ef12e6cc3a286f367abf9
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2022-12-01 14:41:56 +0000

    revered project to working state, with all assets

--- Project Capybara/Assets/Scripts/Pickup.cs
+++ Project Capybara/Assets/Scripts/Pickup.cs
@@ -1,30 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public abstract class Pickup : MonoBehaviour
 {
-    public Pickup()
-    {
+    public TextMeshProUGUI powerupText;
+    public TextMeshProUGUI powerupNameText;
 
-    }
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        powerupText = GameObject.FindGameObjectWithTag("PowerupText").GetComponent<TextMeshProUGUI>();
+
+        powerupText.faceColor = new Color32(255, 255, 255, 0);
+
+        powerupNameText = GameObject.FindGameObjectWithTag("PowerupNameText").GetComponent<TextMeshProUGUI>();
+
+        powerupNameText.faceColor = new Color32(255, 255, 255, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetPowerUpNameText(string t_text)
     {
-        
+        powerupNameText.text = t_text;
+    }
+
+    public void SetPowerUpText(string t_text)
+    {
+        powerupText.text = t_text;
+    }
+
+    public void enableText()
+    {
+        powerupNameText.faceColor = new Color32(255,255,255,225);
+
+        powerupText.faceColor = new Color32(255, 255, 255, 225);
+    }
+
+    public void disableText()
+    {
+        powerupNameText.faceColor = new Color32(255, 255, 255, 0);
+
+        powerupText.faceColor = new Color32(255, 255, 255, 0);
     }
 
     public abstract void outputPowerupName();
 
     public abstract void outputPowerupDescription();
-
-    public abstract void powerupImplementation();
-
-
 }
